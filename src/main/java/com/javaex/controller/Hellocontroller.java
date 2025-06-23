f2a9186616f70275126c86d3a57937a2dab55e3d
ahah2025 <adghty61@naver.com>
2025-06-23 16:05:49 +0900

    2025-06-23

---
+++ src/main/java/com/javaex/controller/Hellocontroller.java
@@ -0,0 +1,32 @@
+package com.javaex.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/user")
+public class Hellocontroller {
+	
+	//필드
+	
+	//생성자
+
+	//메소드gs
+	
+	//메소드일반
+	@RequestMapping(value="/user/list", method= {RequestMethod.GET, RequestMethod.POST})
+	public String select() {
+		System.out.println("guestbook4/list");
+	
+		return "";
+	}	
+	
+	@RequestMapping(value="/user/remove", method= {RequestMethod.GET, RequestMethod.POST})
+	public String delete() {
+		System.out.println("guestbook4/remove");
+		
+		return "/WEB-INF/views/hello.jsp";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/hello.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Insert title here</title>
+	</head>
+	
+	<body>
+		<h1>hello spring boot</h1><br>
+		안녕 스프링 부트야 12:44
+		<br><br><br><br><br><br>
+		guestbook4/remove
+		<br><br><br><br><br><br>
+		guestbook4/list
+		<br><br><br><br><br><br>
+		(^ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;     ^)   <br>
+		 \ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;     /   <br>
+		  \ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;   /
+		 
+		 
+	</body>
+</html>
